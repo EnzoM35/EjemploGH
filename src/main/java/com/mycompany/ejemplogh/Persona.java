535fa69fd8c4e09e96ccc8ce948319cced52c747
Enzo Miranda <enzo792015@gmail.com>
2023-07-25 10:37:08 -0300

    Update Persona.java
    Se agregó el dni y el constructor

--- src/main/java/com/mycompany/ejemplogh/Persona.java
+++ src/main/java/com/mycompany/ejemplogh/Persona.java
@@ -9,5 +9,20 @@ package com.mycompany.ejemplogh;
  * @author enzoa
  */
 public class Persona {
+    private String nombre;
+    private int dni;
+
+    public Persona(String nombre, int dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
 }
